d49c875faa3b776156b92c6a0adf93d7b9135b29
DarthSidM <mauryasiddharth14@gmail.com>
2025-06-01 09:09:34 +0530

    compressed images logic added

--- backend/controllers/categoryGetter.js
+++ backend/controllers/categoryGetter.js
@@ -1,28 +1,30 @@
 const Category = require("../models/categories");
 
-exports.getCategories = async (req,res) =>{
-    try{
+exports.getCategories = async (req, res) => {
+    try {
         const allCategories = await Category.find();
 
-        if(!allCategories || allCategories.length === 0){
+        if (!allCategories || allCategories.length === 0) {
             return res.status(404).json({
-                message: "no categories found",
+                message: "No categories found",
                 success: false
             });
         }
+
         const categoryNames = allCategories.map(category => category.categoryName);
-        // If Categories exist, return them
-        console.log(categoryNames)
+        const categoryIds = allCategories.map(category => category._id.toString());
+
         return res.status(200).json({
             message: "Categories fetched successfully",
             success: true,
-            data: categoryNames
+            data: categoryNames,
+            categoryIds: categoryIds
         });
-    }catch(error){
+    } catch (error) {
         console.error("Error fetching categories: ", error);
         return res.status(500).json({
             message: `Internal server error: ${error.message}`,
             success: false
         });
     }
-}
+};
